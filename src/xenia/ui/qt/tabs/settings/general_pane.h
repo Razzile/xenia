b2bce65ce82aecaa9e2c5ed487bb6bc78b283182
Satori <comfycallum@gmail.com>
2019-09-29 15:16:19 +0100

    Initial work on general settings pane

--- src/xenia/ui/qt/tabs/settings/general_pane.h
+++ src/xenia/ui/qt/tabs/settings/general_pane.h
@@ -7,12 +7,20 @@ namespace xe {
 namespace ui {
 namespace qt {
 
+class XGroupBox;
+
 class GeneralPane : public SettingsPane {
   Q_OBJECT
  public:
   explicit GeneralPane() : SettingsPane(0xE713, "General") {}
 
   void Build() override;
+
+ private:
+  XGroupBox* CreateGeneralGroupBox();
+  XGroupBox* CreateUpdateGroupBox();
+  XGroupBox* CreateWindowGroupBox();
+  XGroupBox* CreateLogGroupBox();
 };
 
 }  // namespace qt
